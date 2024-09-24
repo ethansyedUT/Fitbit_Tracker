894d3e9f9745c6c7a11e41a2f714c83cc7f3ce5d
Ethan Syed <ethansyed@utexas.edu>
2024-09-24 17:22:51 -0500

    Fix empty file + add file in out

---
+++ Lab3.srcs/sim_1/new/FitBit_Tracker_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2024 04:46:33 PM
+// Design Name: 
+// Module Name: FitBit_Tracker_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FitBit_Tracker_tb(
+
+    );
+endmodule
--- Lab3.srcs/sources_1/new/system.v
+++ Lab3.srcs/sources_1/new/system.v
@@ -21,6 +21,19 @@
 
 
 module system(
-
+    input clk,
+    input [3:0] sw,
+    output [6:0] seg,
+    output [0:0] led
     );
+    //sw[3:2] = Mode
+    //sw[1] = Reset
+    //sw[0] = Start
+    
+    //led[0] - SI (>9999 step overflow)
+    
+    // Initialize a pulse generator module
+    // Initialize a fitbit tracker module
+    // Take output of fitbit tracker and feed into BCD
+    // Feed output of BCD into 7-seg display MUX
 endmodule
